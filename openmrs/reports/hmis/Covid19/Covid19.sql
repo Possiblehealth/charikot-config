e4a8bbef785610895c6f3eb4bbaa0acf08a1cb97
suruchee <suruchee11@gmail.com>
2020-04-17 14:18:14 +0545

    all yes changed to TRUE for covid queries

--- openmrs/reports/hmis/Covid19/Covid19.sql
+++ openmrs/reports/hmis/Covid19/Covid19.sql
@@ -151,7 +151,7 @@ FROM
         AND cn1.name IN ('Coivd-Sample collected')
     INNER JOIN concept_name cn2 ON o1.value_coded = cn2.concept_id
         AND cn2.concept_name_type = 'FULLY_SPECIFIED'
-		AND cn2.name IN ('Yes')
+		AND cn2.name IN ('TRUE')
     INNER JOIN encounter e ON o1.encounter_id = e.encounter_id
     INNER JOIN person p1 ON o1.person_id = p1.person_id
     WHERE
@@ -170,7 +170,7 @@ FROM
         AND cn1.name IN ('Covid-Suspection')
     INNER JOIN concept_name cn2 ON o1.value_coded = cn2.concept_id
         AND cn2.concept_name_type = 'FULLY_SPECIFIED'
-		AND cn2.name IN ('Yes')
+		AND cn2.name IN ('TRUE')
     INNER JOIN encounter e ON o1.encounter_id = e.encounter_id
     INNER JOIN person p1 ON o1.person_id = p1.person_id
     WHERE
@@ -241,7 +241,7 @@ FROM
         AND cn1.name IN ('Coivd-Sample collected')
     INNER JOIN concept_name cn2 ON o1.value_coded = cn2.concept_id
         AND cn2.concept_name_type = 'FULLY_SPECIFIED'
-		AND cn2.name IN ('Yes')
+		AND cn2.name IN ('TRUE')
     INNER JOIN encounter e ON o1.encounter_id = e.encounter_id
     INNER JOIN person p1 ON o1.person_id = p1.person_id
     WHERE
