9020354f047d96b9c591d71b7314594ee6bf7d37
root <root@localhost.localdomain>
2021-09-26 17:50:49 +0545

    print update for charikot from charikot

--- openmrs/apps/registration/supplementalPaperLayout/js/printwala.js
+++ openmrs/apps/registration/supplementalPaperLayout/js/printwala.js
@@ -9,9 +9,25 @@ try {
     adDate = parseInt(splitedAD[0])
 
     convertedDate = calendarFunctions.getBsDateByAdDate(adYear, adMonth, adDate)
-    bsDate = convertedDate.bsYear + "-" + convertedDate.bsMonth + "-" + convertedDate.bsDate
+
+    var now = new Date();
+
+    bsDate = convertedDate.bsYear + "-" + convertedDate.bsMonth + "-" + convertedDate.bsDate +" "+now.getHours() +":"+now.getMinutes();
     // alert(bsDate)
     $("#todayDate").text(bsDate)
+
+
+    var todayDate = $("#regDate").text()
+    splitedAD1 = todayDate.split("/");
+
+    // alert(splitedAD[2])
+    adYear1 = parseInt(splitedAD1[2])
+    adMonth1 =parseInt(splitedAD1[1])
+    adDate1 = parseInt(splitedAD1[0])
+
+    convertedDate1 = calendarFunctions.getBsDateByAdDate(adYear1, adMonth1, adDate1)
+    bsDate1 = convertedDate1.bsYear + "-" + convertedDate1.bsMonth + "-" + convertedDate1.bsDate
+    $("#regDate").text(bsDate1)
 }
 catch (err) {
     // alert(err)
